d8675f19ce61fb6558091d74d7f7c4eeb46d014a
Max-Polster <git@max-polster.at>
2025-08-03 20:53:14 +0200

    swagger implamentiert

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,12 @@
 import { Elysia } from 'elysia'
+import { swagger } from '@elysiajs/swagger'
 import { routes } from './routes'
 
-const app = new Elysia().use(routes)
+const app = new Elysia()
+  .use(swagger())  // <- Swagger aktivieren
+  .use(routes)     // <- Routen einbinden
 
 app.listen(3000)
 
 console.log('🚀 Server läuft auf http://localhost:3000')
+console.log('📘 Swagger UI: http://localhost:3000/swagger')
